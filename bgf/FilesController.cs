38bb9bb8fb9562dc9734b8d13666811cc4714475
Bruzzknock <srbinsane@hotmail.rs>
2019-04-02 02:39:01 +0200

    TableCells needed

---
+++ bgf/FilesController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace bgf
+{
+    public partial class FilesController : UITableViewController
+    {
+        public FilesController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
