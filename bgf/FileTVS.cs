fa62675666128d13a56820d7c6fd5c557f648c6c
Bruzzknock <srbinsane@hotmail.rs>
2019-05-28 05:48:43 +0200

    hah

--- bgf/FileTVS.cs
+++ bgf/FileTVS.cs
@@ -56,7 +56,7 @@ namespace bgf
             }
             else
             {
-                //NSUrl url = new NSUrl("Aktive Pausengestaltung.mp4", false);
+                //NSUrl url = new NSUrl("Aktive Pausengestaltung.mp4", false)
                 //"https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"));
                 AVPlayer player = new AVPlayer(new NSUrl(files[indexPath.Row].URL));
                 AVPlayerViewController aVPlayerView = new AVPlayerViewController();
